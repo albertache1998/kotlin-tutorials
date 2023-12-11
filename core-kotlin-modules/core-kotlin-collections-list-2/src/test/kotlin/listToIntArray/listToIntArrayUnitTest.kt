877a25703ada470690fb80e240ba24d605a66b48
albertache1998 <albertache1998@gmail.com>
2023-12-11 02:42:01 +0100

    added unit tests

---
+++ core-kotlin-modules/core-kotlin-collections-list-2/src/test/kotlin/listToIntArray/listToIntArrayUnitTest.kt
@@ -0,0 +1,64 @@
+package listToIntArray
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class listToIntArrayUnitTest {
+
+    @Test
+    fun `convert integer list to integer array using for loop`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expected = intArrayOf(1, 2, 3, 4, 5)
+        val result = IntArray(list.size)
+        for (i in list.indices) {
+            result[i] = list[i]
+        }
+
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `convert integer list to integer array using the intarray constructor`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expected = intArrayOf(1, 2, 3, 4, 5)
+        val result = IntArray(list.size) { i -> list[i] }
+
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `convert integer list to integer array using intArray() method`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expected = intArrayOf(1, 2, 3, 4, 5)
+        val result = list.toIntArray()
+
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `convert integer list to integer array using map() method`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expected = intArrayOf(1, 2, 3, 4, 5)
+        val result = list.map { it }.toIntArray()
+
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `convert integer list to integer array using stream() method`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expected = intArrayOf(1, 2, 3, 4, 5)
+        val result = list.stream().mapToInt { it }.toArray()
+
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `convert integer list to integer array using toTypedArray() method`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val expected = intArrayOf(1, 2, 3, 4, 5)
+        val result = list.toTypedArray().toIntArray()
+
+        assertArrayEquals(expected, result)
+    }
+}
